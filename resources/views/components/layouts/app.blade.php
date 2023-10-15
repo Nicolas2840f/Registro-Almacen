308f5c43d9e0461043c56f8bb30e85f1ed04d855
Nicolas2840f <ncastaneda840@misena.edu.co>
2023-10-14 19:21:01 -0500

    Primer cambio

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="{{$metaDescription ?? 'Default meta description'}}">
+    <title>{{$title}}</title>
+    @vite(['resources/css/app.scss','resources/js/app.js'])
+</head>
+<body>
+    {{$slot}}
+    
+</body>
+</html>
