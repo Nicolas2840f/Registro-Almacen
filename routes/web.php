a687d869e92386767d14f8636d55c53d469ef251
Cristian Morales <pirulomorales20@gmail.com>
2023-10-17 09:34:47 -0500

    Models y Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('usuario', UsuarioController::class, [
+    'names' => 'usuarios',
+    'parameters' => ['usuario' =>  'usuario'],
+]);
+
+
+
 Route::view('/reset','resetPassword')->name("reset");
 Route::view('/register','registerUser')->name("register");
