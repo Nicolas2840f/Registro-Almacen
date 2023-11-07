4dad4934b142d59737d9eea4a66bfca57eb65f12
Cristian Morales <pirulomorales20@gmail.com>
2023-11-07 11:37:27 -0500

    Main

--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -2,4 +2,9 @@
     {{ Auth::user()->rolUsuario }} ---- {{ Auth::user()->rol->descripcionRol }}
     <br>
     {{ Auth::user()->tipoDocumentoUsuario }} ---- {{ Auth::user()->tipoDocumento->descripcionTipoDocumento }}
+
+
+
+
+    {{ Auth::user() }}
 </x-layouts.app>
