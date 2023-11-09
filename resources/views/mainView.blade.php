a8f20a1403e3ec8386b946c0a98eeb65d6fcbf80
Cristian-F-M <cfmorales.diaz@gmail.com>
2023-11-09 10:56:30 -0500

    Logo

--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -2,11 +2,13 @@
     <div>
         <div>
             <form action="" method="post">
+                @csrf
                 <input type="search">
                 <input type="hidden">
                 <span><Strong>Nombre: </Strong>Cristian Morales</span>
                 <span><Strong>Documento: </Strong>1101752630</span>
-                <span><Strong>Correo: </Strong>cfmorales.diaz@gmail.com</span>
+                <span><Strong>Entrada: </Strong>Null</span>
+                <span><strong>Salida: </strong>Null</span>
 
                 <div>
                     <button>Entrada</button>
