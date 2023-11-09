b70eb487ab11a94350f0bd9c2ebb6f06216bbf2d
Cristian Morales <pirulomorales20@gmail.com>
2023-11-09 10:28:35 -0500

    main

--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -1,7 +1,18 @@
 <x-layouts.app title="MainView" meta-description="MainView">
-    {{ Auth::user()->rolUsuario }} ---- {{ Auth::user()->rol->descripcionRol }}
-    <br>
-    {{ Auth::user()->tipoDocumentoUsuario }} ---- {{ Auth::user()->tipoDocumento->descripcionTipoDocumento }}
-    
-    {{ Auth::user() }}
+    <div>
+        <div>
+            <form action="" method="post">
+                <input type="search">
+                <input type="hidden">
+                <span><Strong>Nombre: </Strong>Cristian Morales</span>
+                <span><Strong>Documento: </Strong>1101752630</span>
+                <span><Strong>Correo: </Strong>cfmorales.diaz@gmail.com</span>
+
+                <div>
+                    <button>Entrada</button>
+                    <button>Salida</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-layouts.app>
